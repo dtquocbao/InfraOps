856b924f6717158dcbba9956d6cfa4f01dd55ac1
Bao Dang <dtquocbao@gmail.com>
2026-07-03 10:32:00 -0500

    fix: tsconfig

--- packages/ai-tools/jest.config.js
+++ packages/ai-tools/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testRegex: '.*\\.test\\.ts$',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }],
+  },
 };
